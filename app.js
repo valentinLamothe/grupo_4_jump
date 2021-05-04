a2355849858be501172d39dc9f473287c23a30a2
Your Name <you@example.com>
2021-05-04 04:51:29 -0300

    correccion ruta express y vision movil carrito

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 
-
 const express = require('express');
-const path = require('path');
- 
 const app = express();
+const path = require('path');
  
 const publicPath = path.resolve(__dirname,'./public');
 app.use(express.static(publicPath));
- 
+
+
+
 app.listen(3000, () => {
    console.log('Servidor corriendo en el puerto 3000');
 });
  
+/*arreglado mediante get, el problema de ruteo*/
+app.get('/public', (req, res)=> {
+   res.sendFile(path.resolve(__dirname,'./public'))
+})
+
 app.get('/', (req, res)=> {
    res.sendFile(path.resolve(__dirname,'views/index.html'))
 })
