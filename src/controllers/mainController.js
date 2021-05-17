0e6476dd5ed38991d78d4bb85d2bf8d8c126107b
nahuelmina <nahuelmina@gmail.com>
2021-05-17 19:54:46 -0300

    se agregó el formulario de creación y edición de producto

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,21 +1,27 @@
 const path = require('path');
 
 const mainController = {
-    index: (req, res) => {
-        res.render('index');
-    },
-    login: (req, res) => {
-        res.render('login');
-    },
-    productCart: (req, res) => {
-        res.render('productCart');
-    },
-    productDetail: (req, res) => {
-        res.render('productDetail');
-    },
-    register: (req, res) => {
-        res.render('register');
-    }
-}
+	index: (req, res) => {
+		res.render('index');
+	},
+	login: (req, res) => {
+		res.render('login');
+	},
+	productCart: (req, res) => {
+		res.render('productCart');
+	},
+	productDetail: (req, res) => {
+		res.render('productDetail');
+	},
+	newProduct: (req, res) => {
+		res.render('newProduct');
+	},
+	editProduct: (req, res) => {
+		res.render('editProduct');
+	},
+	register: (req, res) => {
+		res.render('register');
+	}
+};
 
 module.exports = mainController;
--- src/routers/main.js
+++ src/routers/main.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const mainController = require('../controllers/mainController')
+const mainController = require('../controllers/mainController');
 
 var router = express.Router();
 
@@ -7,6 +7,8 @@ router.get('/', mainController.index);
 router.get('/login', mainController.login);
 router.get('/productCart', mainController.productCart);
 router.get('/productDetail', mainController.productDetail);
+router.get('/newProduct', mainController.newProduct);
+router.get('/editProduct', mainController.editProduct);
 router.get('/register', mainController.register);
 
 module.exports = router;
