a7108547736271e1f2c8f882ba438a556ec50d60
nahuelmina <nahuelmina@gmail.com>
2021-05-29 11:19:21 -0300

    cambios en la vista de detalle de producto

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,30 +1,28 @@
-const path = require("path");
-const express = require("express");
+const path = require('path');
+const express = require('express');
 const fs = require('fs'); /* Requerimos el módulo FS para leer distintos tipos de archivo */
 
-const fileProductsPath = path.join(__dirname, '../../data/products.json') /* Traemos el JSON de productos */
-const products = JSON.parse(fs.readFileSync(fileProductsPath, 'utf-8'))
-
-
-
-
+const fileProductsPath = path.join(__dirname, '../../data/products.json'); /* Traemos el JSON de productos */
+const products = JSON.parse(fs.readFileSync(fileProductsPath, 'utf-8'));
 
 const productsController = {
-  index: (req, res) => {
-      res.render('productsIndex', { products: products } )
-  },
+	index: (req, res) => {
+		res.render('productsIndex', { products: products });
+	},
 
-  detail: (req, res) => {},
+	detail: (req, res) => {
+		res.render('productDetail', { productDetail: products.find((producto) => producto.id == req.params.id) });
+	},
 
-  create: (req, res) => {},
+	create: (req, res) => {},
 
-  store: (req, res) => {},
+	store: (req, res) => {},
 
-  edit: (req, res) => {},
+	edit: (req, res) => {},
 
-  update: (req, res) => {},
+	update: (req, res) => {},
 
-  destroy: (req, res) => {},
+	destroy: (req, res) => {}
 };
 
 module.exports = productsController;
--- src/routers/products.js
+++ src/routers/products.js
@@ -3,14 +3,13 @@ const router = express.Router();
 
 const productsController = require('../controllers/productsController');
 
-
-
-router.get('/', productsController.index);//listado productos
-/* router.get('/create', productController.create);//creacion de productos
-router.get('/:id', productController.detail);//detalle individual producto
-router.post('/products', productController.store);//direccion de creacion(donde apunta el formulario)
-router.get('/:id/edit', productController.edit);//formulario edicion producto
-router.put('/:id', productController.update);// accion de edicion (donde apunta el formulario)
-router.delete('/:id',productController.delete);//borra producto especifico */
-
+router.get('/', productsController.index); //listado productos
+router.get('/:id/', productsController.detail); //detalle individual producto
+/*
+router.get('/create', productController.create); //creacion de productos
+router.post('/products', productController.store); //direccion de creacion(donde apunta el formulario)
+router.get('/:id/edit', productController.edit); //formulario edicion producto
+router.put('/:id', productController.update); // accion de edicion (donde apunta el formulario)
+router.delete('/:id', productController.delete); //borra producto especifico
+*/
 module.exports = router;
