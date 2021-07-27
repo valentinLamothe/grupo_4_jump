6294a822f69d0f8441fbd129af0df4e271d523ab
nahuelmina <nahuelmina@gmail.com>
2021-07-26 21:22:56 -0300

    Cambios en el SQL

--- src/data/jump.sql
+++ src/data/jump.sql
@@ -21,6 +21,9 @@ SET time_zone = "+00:00";
 -- Base de datos: `jump`
 --
 
+CREATE DATABASE IF NOT EXISTS `jump` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `jump`;
+
 -- --------------------------------------------------------
 
 --
