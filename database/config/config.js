43ff89e6c5eafb6fc5796e6c71d6d8bbb69c0754
IvanFranco1702 <ivanfrancoarg@gmail.com>
2021-07-28 14:03:21 -0300

    viendo el config

--- database/config/config.js
+++ database/config/config.js
@@ -1,10 +1,9 @@
 module.exports={
   "development": {  
     "username": "root",
-    "password": "root",
+    "password": "",
     "database": "jump",
     "host": "127.0.0.1",
     "dialect": "mysql",
-    "port": '8889'
     }
 }
