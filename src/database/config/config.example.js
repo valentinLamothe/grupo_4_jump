bcd337f46089d47c78939764fc348c4c5a1cad1f
nahuelmina <nahuelmina@gmail.com>
2021-08-09 20:46:59 -0300

    Correcciones en el logueo de usuarios

---
+++ src/database/config/config.example.js
@@ -0,0 +1,10 @@
+module.exports = {
+	development: {
+		username: '',
+		password: '',
+		database: '',
+		host: '',
+		dialect: '',
+		port: ''
+	}
+};
--- src/middlewares/multerMiddleware.js
+++ src/middlewares/multerMiddleware.js
@@ -2,14 +2,14 @@ const multer = require('multer');
 const path = require('path');
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, '../public/img/usersAvatars'))
-    },
-    filename: (req, file, cb) => {
-        cb(null, 'avatar-' + Date.now() + path.extname(file.originalname))
-    }
-})
+	destination: (req, file, cb) => {
+		cb(null, path.join(__dirname, '../../public/img/usersAvatars'));
+	},
+	filename: (req, file, cb) => {
+		cb(null, 'avatar-' + Date.now() + path.extname(file.originalname));
+	}
+});
 
-const uploadFile = multer({storage});
+const uploadFile = multer({ storage });
 
-module.exports = uploadFile
+module.exports = uploadFile;
