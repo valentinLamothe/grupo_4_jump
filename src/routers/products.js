d13e309481f3b164d8a7d1b3293b768f7a93337d
nahuelmina <nahuelmina@gmail.com>
2021-08-17 20:40:46 -0300

    Ajustes en las validaciones del back

--- src/routers/products.js
+++ src/routers/products.js
@@ -3,32 +3,32 @@ const router = express.Router();
 const multer = require('multer');
 const path = require('path');
 
-
-
 // Controlador
 const productsController = require('../controllers/productsController');
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, '../../public/img/productos'))
-    },
-    filename: (req, file, cb) => {
-        cb(null, 'product-' + Date.now() + path.extname(file.originalname))
-    }
-})
+//Middleware
+const productValidator = require('../middlewares/validateProductMiddleware');
 
-const uploadFile = multer({storage})
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, path.join(__dirname, '../../public/img/productos'));
+	},
+	filename: (req, file, cb) => {
+		cb(null, 'product-' + Date.now() + path.extname(file.originalname));
+	}
+});
+
+const uploadFile = multer({ storage });
 
 router.get('/', productsController.index); //listado productos
-router.get('/create/', productsController.create); //creacion de productos
+router.get('/create/', productValidator, productsController.create); //creacion y validación de productos
 
-router.get('/:id/', productsController.detail);// detalle individual producto
-router.get('/:id/edit', productsController.edit);// formulario edicion producto
+router.get('/:id/', productsController.detail); // detalle individual producto
+router.get('/:id/edit', productsController.edit); // formulario edicion producto
 router.delete('/:id', productsController.delete); //borra producto especifico
 
+router.post('/', uploadFile.single('image'), productsController.store); // direccion de creacion(donde apunta el formulario)
 
-router.post('/',uploadFile.single('image'), productsController.store);// direccion de creacion(donde apunta el formulario)
-
-router.put('/:id', productsController.update);  //accion de edicion (donde apunta el formulario)
+router.put('/:id', productsController.update); //accion de edicion (donde apunta el formulario)
 
 module.exports = router;
