e949bdfa6f887800aaa1f6fef24edade45e1bd8f
nahuelmina <nahuelmina@gmail.com>
2021-08-18 20:39:58 -0300

    Ajustes en la validación front del formulario de registro

--- public/js/validations.js
+++ public/js/validations.js
@@ -4,23 +4,74 @@
 
 // Validación del formulario de registro de usuario
 
-let registerUserForm = document.querySelector('form#registerUserForm');
-let emailField = registerUserForm.email;
-let userField = registerUserForm.name;
-let avatarField = registerUserForm.avatar_image;
-let pwdField = registerUserForm.password;
-let submitBtn = document.querySelector('.boton-registrar button');
+// Capturamos los inputs del formulario
+const registerUserForm = document.querySelector('form#registerUserForm');
+const emailField = registerUserForm.email;
+const userField = registerUserForm.name;
+const avatarField = registerUserForm.avatar_image;
+const passwordField = registerUserForm.password;
+const submitBtn = document.querySelector('.boton-registrar button');
 
-let isEmpty = (field) => {
-	return field.value.trim().length === 0;
-};
-
+// Validamos el formulario con la función checkInputs() y evitamos que se envíe con un preventDefault()
 registerUserForm.addEventListener('submit', (e) => {
 	e.preventDefault();
-
-	if (isEmpty(emailField) != '' || isEmpty(userField) != '' || isEmpty(pwdField) != '') {
-		alert('Los campos no pueden estar vacíos');
-	} else {
-		registerUserForm.submit();
-	}
+	checkInputs();
 });
+
+// Función que valida los inputs
+function checkInputs() {
+	// Obtenemos los values de los inputsy les hacemos trim() para eliminar los espacios en blanco
+	const emailValue = emailField.value.trim();
+	const userValue = userField.value.trim();
+	const passwordValue = passwordField.value.trim();
+
+	if (emailValue === '') {
+		showErrorFor(emailField, 'El email no puede estar vacío');
+		return false;
+	} else if (!isEmail(emailValue)) {
+		showErrorFor(emailField, 'No es un email válido');
+		return false;
+	} else {
+		showSuccessFor(emailField);
+	}
+
+	if (userValue === '') {
+		showErrorFor(userField, 'El nombre de usuario no puede estar vacío');
+		return false;
+	} else {
+		showSuccessFor(userField);
+	}
+
+	if (passwordValue === '') {
+		showErrorFor(passwordField, 'La contraseña no puede estar vacía');
+		return false;
+	} else {
+		showSuccessFor(passwordField);
+	}
+
+	registerUserForm.submit();
+}
+
+// Función que muestra los mensajes de error
+function showErrorFor(input, message) {
+	let formField = input.parentElement;
+	let small = formField.querySelector('small.err');
+	formField.className = 'form-field error';
+	small.innerText = message;
+}
+
+// Función que muestra el mensaje de success
+function showSuccessFor(input) {
+	let formField = input.parentElement;
+	formField.className = 'form-field success';
+}
+
+// Función que verifica que el campo no esté vacío
+function isEmpty(field) {
+	return field.value.trim().length === 0;
+}
+
+// Función que verifica que sea un email válido
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
