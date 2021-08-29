6fd5eeb97fc66c24560bc76617cf1d79506234a0
Your Name <you@example.com>
2021-08-29 14:24:02 -0300

    api fase 2

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -1,21 +1,41 @@
 const db = require("../database/models");
-
+const sequelize = db.sequelize;
 const Op = db.Sequelize.Op;
 
 const apiController = {
   users: (req, res) => {
     db.User.findAll().then(function (user) {
-      return res.status(200).json(user);
+      return res.status(200).json({
+        count: user.length,
+        users: user,
+      });
     });
   },
+
   userId: (req, res) => {
     db.User.findByPk(req.params.id).then(function (user) {
       return res.status(200).json(user);
     });
   },
   products: (req, res) => {
+    // pruebas
+   /*  db.Product.findAll().then(function (products)
+
+ */
+
     db.Product.findAll().then(function (products) {
-      return res.status(200).json(products);
+      return res.status(200).json({
+        count: products.length,
+       /*  /* countByCategory:products.filter(
+          category =>
+          
+          ), // "numerito a colorar",
+        /* where: {
+        ,
+       } */  */
+
+        products: products,
+      });
     });
   },
   productId: (req, res) => {
--- src/routers/api.js
+++ src/routers/api.js
@@ -3,8 +3,9 @@ const apiController = require("../controllers/apicontroller");
 
 const router = express.Router();
 
-router.get("/users", apiController.users);
+router.get("/users", apiController.users); //debe dar el count y el array de user
 router.get("/users/:id", apiController.userId);
+
 router.get("/products", apiController.products);
 router.get("/products/:id", apiController.productId);
 
