62260d2c70abaf69df66c379ab94f3c58dfd3266
nahuelmina <nahuelmina@gmail.com>
2021-09-20 22:34:42 -0300

    Script de filtros dinámicos en las vistas

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,15 +1,36 @@
+// CARRITO
+// let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
-	let cart = JSON.parse(localStorage.getItem('cart')) || [];
+// let button = document.querySelector('#addToCart');
+// let carrito1 = document.querySelector('.fa-shopping-cart');
 
-	let button = document.querySelector('#addToCart');
-	let carrito1 = document.querySelector('.fa-shopping-cart');
+// button.addEventListener('click', (e) => {
+// 	e.preventDefault();
+// 	console.log('aniadido');
+// 	cart.push(1);
+// 	localStorage.setItem('carrito', JSON.stringify(cart));
+// 	carrito1.innerText = cart.length;
+// 	console.log(cart.length);
+// });
 
-	
-	button.addEventListener('click', (e) => {
-		e.preventDefault()
-		console.log('aniadido')
-		cart.push(1)
-		localStorage.setItem('carrito', JSON.stringify(cart))
-		carrito1.innerText = cart.length;
-		console.log(cart.length)
-	})
+// FILTROS
+
+if (document.body.classList.contains('products')) {
+	// Capturamos el formulario
+	const brandFilter = document.querySelector('form#brandFilter');
+	let brandSelected = 0;
+
+	brandFilter.addEventListener('submit', (e) => {
+		e.preventDefault();
+		selectBrand();
+	});
+
+	function selectBrand() {
+		let brandOptions = document.getElementById('brand');
+		brandSelected = brandOptions.options[brandOptions.selectedIndex].value;
+
+		if (brandSelected > 0) {
+			window.location.href = '/products/brand/' + brandSelected;
+		}
+	}
+}
