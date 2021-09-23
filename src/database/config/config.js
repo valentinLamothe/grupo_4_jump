f3f941151b1eaa1e83139d1d2d89f0807b8ad258
Valentin Lamothe coulomme <valentinlamothecoulomme@MacBook-Pro-de-Valentin.local>
2021-09-23 10:56:11 -0300

    heroku

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,10 +1,24 @@
 module.exports = {
 	development: {
-		username: 'root',
-		password: 'root',
-		database: 'jump',
-		host: '127.0.0.1',
-		dialect: 'mysql',
-		port: '8889'
-	}
+		'username': 'root',
+		'password': 'root',
+		'database': 'jump',
+		'host': '127.0.0.1',
+		'dialect': 'mysql',
+		'port': '8889'
+	},
+	"test": {
+		"username": "root",
+		"password": null,
+		"database": "database_test",
+		"host": "127.0.0.1",
+		"dialect": "mysql"
+	  },
+	  "production": {
+		"username": "root",
+		"password": null,
+		"database": "database_production",
+		"host": "127.0.0.1",
+		"dialect": "mysql"
+	  }
 };
